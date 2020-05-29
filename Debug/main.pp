829a3dc803cc98339a2208005c50875194c10e93
romanjrdykyj <romanjrdykyj@gmail.com>
2020-05-29 18:10:18 +0200

    Projekt_uczelnia_widma

---
+++ Debug/main.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../include/dsplib.h
+main.obj: ../include/tms320.h
+main.obj: ../include/testsignal.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/math.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/float.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/access.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/elfnames.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/mathf.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/mathl.h
+main.obj: C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/unaccess.h
+
+../main.c: 
+../include/dsplib.h: 
+../include/tms320.h: 
+../include/testsignal.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/math.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/float.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/access.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/elfnames.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/mathf.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/mathl.h: 
+C:/ti/ccsv5/tools/compiler/c5500_4.4.1/include/unaccess.h: 
